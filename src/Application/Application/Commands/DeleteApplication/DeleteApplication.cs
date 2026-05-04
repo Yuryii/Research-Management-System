021688b2af1b63c99e4dcc40fd05e859eb3f0048
Lê Tiến Dũng <141888173+Yuryii@users.noreply.github.com>
2026-05-04 23:17:31 +0700

    feat: add delete file in store when delete application

--- src/Application/Application/Commands/DeleteApplication/DeleteApplication.cs
+++ src/Application/Application/Commands/DeleteApplication/DeleteApplication.cs
@@ -6,22 +6,34 @@ public record DeleteApplicationCommand(Guid Id) : IRequest;
 public class DeleteApplicationCommandHandler : IRequestHandler<DeleteApplicationCommand>
 {
     private readonly IApplicationDbContext _context;
+    private readonly IFileService _fileService;
 
-    public DeleteApplicationCommandHandler(IApplicationDbContext context)
+    public DeleteApplicationCommandHandler(IApplicationDbContext context, IFileService fileService)
     {
         _context = context;
+        _fileService = fileService;
     }
 
     public async Task Handle(DeleteApplicationCommand request, CancellationToken cancellationToken)
     {
         var entity = await _context.Applications
+            .Include(a => a.ApplicationFiles)
             .Where(a => a.Id == request.Id)
             .SingleOrDefaultAsync(cancellationToken);
 
         Guard.Against.NotFound(request.Id, entity, "Application not found.");
 
+        var fileIds = entity.ApplicationFiles
+            .Select(af => af.FileId)
+            .ToList();
+
         _context.Applications.Remove(entity);
 
         await _context.SaveChangesAsync(cancellationToken);
+
+        foreach (var fileId in fileIds)
+        {
+            await _fileService.DeleteFileAsync(fileId, cancellationToken);
+        }
     }
 }
