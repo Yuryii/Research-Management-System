08d53293a83f248175318443a64bf9e79e1261af
Lê Tiến Dũng <141888173+Yuryii@users.noreply.github.com>
2026-05-05 11:35:27 +0700

    feat: add authorize teacher for add application

--- src/Application/Application/Commands/CreateApplication/CreateApplicationCommand.cs
+++ src/Application/Application/Commands/CreateApplication/CreateApplicationCommand.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using RMS.Application.Common.Models;
+using RMS.Application.Common.Security;
+using RMS.Domain.Constants;
 using RMS.Domain.Enums;
 
 namespace RMS.Application.Application.Commands.CreateApplication;
 
+[Authorize(Roles = Roles.Teacher)]
 public record CreateApplicationCommand : IRequest<Guid>
 {
     public required string Title { get; init; }
